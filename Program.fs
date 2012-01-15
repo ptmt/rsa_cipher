981e67cde7ca95cd6eede143321023adbfba7057
unknownexception <unknownliveid@hotmail.com>
2012-01-15 15:56:43 +0700

    primality test

--- Program.fs
+++ Program.fs
@@ -1,35 +1,17 @@
-﻿// Learn more about F# at http://fsharp.net
+﻿module rsa_cipher
 
+open millerrabin
+open rsa_helpers
 open System.Numerics
 
-let rec toBinary (n:BigInteger) r =
-    if n > BigInteger.Zero then
-        toBinary (n/(BigInteger 2)) (r @ [n% (BigInteger 2)])
-    else
-        r
+let number_to_check = BigInteger(1259)
+let isPrime = isPrimeW [2I;3I] // Two witnesses
+//let p = pown 2I 4423 - 1I // 20th Mersenne prime. 1,332 digits
+isPrime number_to_check |> printfn "%b"
 
-let test (a:BigInteger) (n:BigInteger) =
-    let (b:List<BigInteger>) = toBinary (n - BigInteger.One) []
-    let mutable d = BigInteger.One
-    let mutable Prime = false
-    let CheckList = [0 .. b.Length-1 ] |> List.rev
-    for i in CheckList do
-        let x = d
-        d <- (d*d) % n
-        if (d = BigInteger.One && x <> BigInteger.One && x <> n-BigInteger.One) then
-            Prime <- true // complex
-        if b.[i] = BigInteger.One then
-            d <- (d*a) % n
-    if d <> BigInteger.One then
-        Prime <- true //complex
-    Prime //if its still false then prime
+let rsa_key_length = 1024
+let primeNumberA = rsa_helpers.GenPrime rsa_key_length
+let primeNumberB = rsa_helpers.GenPrime rsa_key_length
 
-let Rand = new System.Random()
 
-let MillerRabin (n:int) (s:int) =
-    let mutable Prime = true
-    for j in [1 .. s+1] do
-        let a = Rand.Next(1, n - 1)
-        if (test (BigInteger a) (BigInteger n)) then
-            Prime <- false
-    Prime
+  
---
+++ rsa_helpers.fs
@@ -0,0 +1,34 @@
+﻿module rsa_helpers
+
+open System.Numerics
+
+let Rand = new System.Random()
+
+let rec BigIntegerSize n =
+    if n > BigInteger.Zero then
+        BigIntegerSize(n>>>1) + 1
+    else
+       0
+
+let GetRandomBit =    
+    Rand.Next(0,2)
+
+let rec GenRandomCandidate (size:int, i:int, c:BigInteger) =
+    if (i=size) then c
+    else GenRandomCandidate (size, (i+1), (c<<<1)+BigInteger(GetRandomBit))
+
+let PrimalityTestWrapper (n:BigInteger) =    
+    if n % BigInteger(2) = BigInteger.Zero then
+        false
+    else 
+        millerrabin.isPrimeW [2I;3I] n
+
+let rec SearchPrime c =    
+    match PrimalityTestWrapper c  with
+    | true -> c
+    | _ -> SearchPrime (c + BigInteger.One)
+      
+// сгенирировать случайное простое (псевдопростое) число заданного размера size 
+let GenPrime size =
+    SearchPrime (GenRandomCandidate(size, 1, BigInteger.One))
+
